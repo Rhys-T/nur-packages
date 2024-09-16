92282e1fd990475174b6876c514e91675dbee61a
Rhys-T <108157737+Rhys-T@users.noreply.github.com>
2024-09-16 12:51:33 -0400

    lix-game: init at 0.10.26

--- default.nix
+++ default.nix
@@ -8,13 +8,17 @@
 
 { pkgs ? import <nixpkgs> { } }:
 
-{
+rec {
   # The `lib`, `modules`, and `overlays` names are special
   lib = import ./lib { inherit pkgs; }; # functions
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  # example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
+  lix-game = pkgs.callPackage ./pkgs/lix-game { inherit lix-game-assets lix-game-music; };
+  lix-game-assets = pkgs.callPackage ./pkgs/lix-game/assets.nix { inherit lix-game; };
+  lix-game-music = pkgs.callPackage ./pkgs/lix-game/music.nix { inherit lix-game; };
+  lix-game-server = pkgs.callPackage ./pkgs/lix-game/server.nix { inherit lix-game; };
 }
