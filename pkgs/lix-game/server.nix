574d0dd0a822d2de011c98c39697c8b94ca7aa56
Rhys-T <108157737+Rhys-T@users.noreply.github.com>
2024-09-17 15:23:58 -0400

    Set maintainer

--- pkgs/lix-game/server.nix
+++ pkgs/lix-game/server.nix
@@ -16,7 +16,7 @@
     runHook postInstall
     '';
     meta = {
-        inherit (lix-game.meta) homepage;
+        inherit (lix-game.meta) homepage maintainers;
         license = lib.licenses.cc0;
         mainProgram = "lixserv";
     };
