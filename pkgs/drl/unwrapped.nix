4ba1280076ff7e6908179d5d1a54a300efab8cba
Rhys-T <108157737+Rhys-T@users.noreply.github.com>
2024-11-23 18:14:08 -0500

    drl: try patching FMOD bindings _correctly_

--- pkgs/drl/unwrapped.nix
+++ pkgs/drl/unwrapped.nix
@@ -76,11 +76,11 @@ in stdenv.mkDerivation rec {
                 /GetSymbolExt\s*:=/! d
             }
         ' "$FPCVALKYRIE_ROOT"/libs/vgl3library.pas
-    '' + lib.optionalString (true || !stdenv.hostPlatform.isx86_64) ''
+    '' + lib.optionalString (!stdenv.hostPlatform.isx86_64) ''
         sed -E -i '
             s@Set8087CW@// &@g
             /LoadFMOD/,/^end;/ {
-                /function GetSymbol/ i\
+                /^begin/ a\
                 raise ELibraryError.Create('\'''FMOD support disabled - see <https://github.com/chaosforgeorg/doomrl/issues/46#issuecomment-2453210202>'\''');
             }
         ' "$FPCVALKYRIE_ROOT"/libs/vfmod2library.pas
