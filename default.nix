715a30c00e36a56274de78d1259c39a7a21d5003
Rhys-T <108157737+Rhys-T@users.noreply.github.com>
2024-11-27 19:34:39 -0500

    ci: darwin test: oops - remove those flags, they don't help and they clutter up the logs

--- default.nix
+++ default.nix
@@ -162,11 +162,11 @@ in {
         #     echo ']]]]]]]]]]]]]]]]]]]]]] env lines containing apple-sdk'
         #     exit 1
         # '';
-        postPatch = (old.postPatch or "") + ''
-            NIX_CFLAGS_COMPILE+=" -isysroot $SDKROOT"
-            NIX_CFLAGS_COMPILE+=" -idirafter $SDKROOT/usr/include"
-            NIX_CFLAGS_COMPILE+=" -iframework $SDKROOT/System/Library/Frameworks"
-        '';
+        # postPatch = (old.postPatch or "") + ''
+        #     NIX_CFLAGS_COMPILE+=" -isysroot $SDKROOT"
+        #     NIX_CFLAGS_COMPILE+=" -idirafter $SDKROOT/usr/include"
+        #     NIX_CFLAGS_COMPILE+=" -iframework $SDKROOT/System/Library/Frameworks"
+        # '';
         PS4 = "+(\${BASH_SOURCE}:\${LINENO}): \${FUNCNAME[0]:+\${FUNCNAME[0]}(): }";
         NIX_DEBUG = 7;
         NIX_LDFLAGS = (old.NIX_LDFLAGS or "") + " -t";
