c605df3ee32cf4017c24d5f5557da93cba7d1bed
Rhys-T <108157737+Rhys-T@users.noreply.github.com>
2025-02-12 14:21:39 -0500

    tuxemon: refactor: shared data; mods wrapper

--- pkgs/tuxemon/git.nix
+++ pkgs/tuxemon/git.nix
@@ -2,4 +2,5 @@
     version = "0.4.34-unstable-2025-01-25";
     rev = "19708725f8b2d939e39b726f49a8a078eba8bf32";
     hash = "sha256-Bl1YFTqznRG8iswwH6gXfPMizJldTdpdfCUhoruZsEk=";
+    dataHash = "sha256-ThHuVqbkW3DivoCEcx6soOaVy1Uxucz4rSGmJPiRZCk=";
 }
