afab71f6523c075b05f18a86a693e611f8feabda
Rhys-T <108157737+Rhys-T@users.noreply.github.com>
2025-02-27 20:16:39 -0500

    picolisp: init at ${pkgs.picolisp.version}; picolisp-rolling: init at 25.2.27

--- default.nix
+++ default.nix
@@ -132,6 +132,43 @@ in {
         hbmame = self.pacifi3d-hbmame.romsFromXML;
     };
     
+    # Backported fixes from https://github.com/NixOS/nixpkgs/pull/385459
+    picolisp = let
+        inherit (pkgs) lib picolisp hostPlatform;
+        picolisp' = if lib.hasInfix "cd src" (picolisp.preBuild or "") then picolisp else picolisp.overrideAttrs (old: {
+            preBuild = (old.preBuild or "") + ''
+                cd src
+            '' + lib.optionalString hostPlatform.isDarwin ''
+                # Flags taken from instructions at: https://picolisp.com/wiki/?alternativeMacOSRepository
+                makeFlagsArray+=(
+                    SHARED='-dynamiclib -undefined dynamic_lookup'
+                )
+            '';
+            buildPhase = null;
+            installPhase = builtins.replaceStrings ["--replace "] ["--replace-fail "] old.installPhase;
+        });
+    in lib.addMetaAttrs ({
+        description = (picolisp.meta.description or "PicoLisp") + " (fixed for macOS/Darwin)";
+    }) picolisp';
+    
+    picolisp-rolling = let
+        inherit (pkgs) lib fetchFromGitea;
+        inherit (self) picolisp;
+        picolisp' = picolisp.overrideAttrs (old: {
+            version = "25.2.27";
+            src = fetchFromGitea {
+                domain = "git.envs.net";
+                owner = "mpech";
+                repo = "pil21";
+                rev = "48ed9af5bec4b6d62b13d81a4f4ae1fafe79263a";
+                hash = "sha256-ujqTJjlzcwNyP6IqMVdIgX5eP0xAcA8RGG6r0Hphk9s=";
+            };
+            sourceRoot = null;
+        });
+    in lib.addMetaAttrs ({
+        description = lib.replaceStrings [") ("] ["; "] ((picolisp.meta.description or "PicoLisp") + " (rolling release)");
+    }) picolisp';
+    
     konify = callPackage ./pkgs/konify {};
     
     asciiportal = callPackage ./pkgs/asciiportal {};
