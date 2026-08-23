d915d7d3c0bbc1bc8a40bda65dba7e29012e3417
Rhys-T <108157737+Rhys-T@users.noreply.github.com>
2026-08-23 13:29:50 -0400

    auto update debug

--- update-wrapper.py
+++ update-wrapper.py
@@ -6,5 +6,6 @@ with open(realUpdate, 'r') as f:
 	realUpdateCode = f.read()
 shellPath = pathlib.Path(nixpkgsPath).resolve()/'shell.nix'
 updateCode = realUpdateCode.replace('nixpkgs_root + "/shell.nix"', repr(str(shellPath)))
+updateCode = updateCode.replace('changes = json.loads(update_info)', 'print(f"{package!r}: {update_info!r}"); changes = json.loads(update_info)')
 updateCompiled = compile(updateCode, realUpdate, 'exec')
 exec(updateCompiled)
