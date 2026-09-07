53194e7cba67f734cc2bba219627e212ce0d3a22
Rhys-T <108157737+Rhys-T@users.noreply.github.com>
2026-09-07 11:36:09 -0400

    hbmame: fix typo

--- pkgs/mame/hbmame/default.nix
+++ pkgs/mame/hbmame/default.nix
@@ -70,7 +70,7 @@
               substituteInPlace "$file" \
                 --subst-var-by mamePath "$out/opt/mame"
             done
-        ''] old.postPatch ++ ''
+        ''] old.postPatch + ''
             substituteInPlace src/lib/util/archiver.cpp --replace-fail '#include <zutil.h>' '//#include <zutil.h>'
         '';
         makeFlags = map (x: if x == "TOOLS=1" then "TOOLS=0" else x) (old.makeFlags or []) ++ ["TARGET=hbmame"];
